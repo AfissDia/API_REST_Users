07a592265c2b6bae0523b91590c0b42cb79e5bf3
AfissDia <afissdia07@gmail.com>
2022-02-12 21:21:33 +0000

    first commit

---
+++ src/main/java/com/dia/cartes/Cartes1Application.java
@@ -0,0 +1,13 @@
+package com.dia.cartes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cartes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cartes1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dia/cartes/Cartes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dia.cartes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cartes1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
